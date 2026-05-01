c8151d873bd1a1a9221227963affc6a00101cd6d
ser0kim <srkim0728@gmail.com>
2026-05-01 22:39:16 +0900

    [feat] 전체 레이아웃 설정

---
+++ KimSereong-assignment/KimSereong-assignment/Feature/Subscribe/Layout/CompositionalLayout.swift
@@ -0,0 +1,140 @@
+//
+//  SubscribeViewController.swift
+//  KimSereong-assignment
+//
+//  Created by 김세령 on 4/29/26.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+
+struct CompositionalLayout{
+    
+    static func createLayout() -> UICollectionViewLayout {
+        return UICollectionViewCompositionalLayout { (sectionNumber, _) -> NSCollectionLayoutSection? in
+            
+            if sectionNumber == 0 {
+                
+                let item = NSCollectionLayoutItem(
+                    layoutSize: .init(
+                        widthDimension: .fractionalWidth(1),
+                        heightDimension: .fractionalHeight(1)
+                    )
+                )
+                
+                item.contentInsets.leading = 13
+                
+                
+                let group = NSCollectionLayoutGroup.horizontal(
+                    layoutSize: .init(
+                        widthDimension: .fractionalWidth(0.8), heightDimension: .absolute(399)
+                    ), subitems: [item]
+                )
+                
+                let section = NSCollectionLayoutSection(group: group)
+                
+                section.orthogonalScrollingBehavior = .paging
+                section.contentInsets = .init(top: 20, leading: 20, bottom: 30, trailing: 20)
+                
+                return section
+            } else if sectionNumber == 2 || sectionNumber == 3 {
+                
+                let item = NSCollectionLayoutItem(
+                    layoutSize: .init(
+                        widthDimension: .fractionalWidth(1),
+                        heightDimension: .fractionalHeight(1)
+                    )
+                )
+                
+                item.contentInsets.leading = 13
+                
+                let group = NSCollectionLayoutGroup.horizontal(
+                    layoutSize: .init(
+                        widthDimension: .fractionalWidth(0.3), heightDimension: .absolute(153)
+                    ), subitems: [item]
+                )
+                
+                
+                let section = NSCollectionLayoutSection(group: group)
+                
+                section.orthogonalScrollingBehavior = .continuous
+                
+                let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(60))
+                
+                let header = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerSize, elementKind: UICollectionView.elementKindSectionHeader, alignment: .top)
+                section.boundarySupplementaryItems = [header]
+                section.contentInsets = .init(top: 10, leading: 14, bottom: 25, trailing: 0)
+                section.interGroupSpacing = 12
+                
+                return section
+            } else if sectionNumber == 1 {
+                let item = NSCollectionLayoutItem(
+                    layoutSize: .init(
+                        widthDimension: .fractionalWidth(1),
+                        heightDimension: .fractionalHeight(1)
+                    )
+                )
+                
+                item.contentInsets.leading = 12
+                
+                let group = NSCollectionLayoutGroup.horizontal(
+                    layoutSize: .init(
+                        widthDimension: .fractionalWidth(0.8), heightDimension: .absolute(180)
+                    ), subitems: [item]
+                )
+                
+                
+                let section = NSCollectionLayoutSection(group: group)
+                
+                section.orthogonalScrollingBehavior = .continuous
+                
+                let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(60))
+                
+                let header = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerSize, elementKind: UICollectionView.elementKindSectionHeader, alignment: .top)
+                section.boundarySupplementaryItems = [header]
+                section.contentInsets = .init(top: 10, leading: 14, bottom: 25, trailing: 0)
+                section.interGroupSpacing = 12
+                
+                return section
+            } else if sectionNumber == 4 {
+                let item = NSCollectionLayoutItem(
+                    layoutSize: .init(
+                        widthDimension: .fractionalWidth(1),
+                        heightDimension: .fractionalHeight(1)
+                    )
+                )
+                
+                item.contentInsets.leading = 12
+                
+                let group = NSCollectionLayoutGroup.horizontal(
+                    layoutSize: .init(
+                        widthDimension: .fractionalWidth(0.6), heightDimension: .absolute(185)
+                    ), subitems: [item]
+                )
+                
+                
+                let section = NSCollectionLayoutSection(group: group)
+                
+                section.orthogonalScrollingBehavior = .continuous
+                section.contentInsets = .init(top: 10, leading: 14, bottom: 25, trailing: 0)
+                section.interGroupSpacing = 12
+                
+                let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(60))
+                
+                let header = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerSize, elementKind: UICollectionView.elementKindSectionHeader, alignment: .top)
+                section.boundarySupplementaryItems = [header]
+                section.contentInsets = .init(top: 10, leading: 14, bottom: 25, trailing: 0)
+                section.interGroupSpacing = 12
+                
+                return section
+                
+            }
+            
+            return nil
+        }
+    }
+}
+
